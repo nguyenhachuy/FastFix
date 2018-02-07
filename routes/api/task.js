727190c10feb6296b65eb67f9a72800da2e52e66
Ryosaku <hiwaryo@gmail.com>
2018-02-06 20:54:11 -0800

    fixed router

--- routes/api/task.js
+++ routes/api/task.js
@@ -3,25 +3,25 @@ const taskController = require("../../controllers/taskController");
 
 // Matches with "/api/task"
 router
-  .route("/")
+  .route("task/")
   .get(taskController.findAll)
   .post(taskController.create);
 
 // Matches with "/api/task/:id"
 router
-  .route("/:id")
+  .route("task/:id")
   .get(taskController.findById)
   .put(taskController.update)
   .delete(taskController.remove);
 
 // Matches with "/api/task/user/:id"
 router
-  .route("/user/:id")
+  .route("task/user/:id")
   .get(taskController.findByUserId)
 
 // Matches with "/api/task/contractor/:id"
 router
-  .route("/contractor/:id")
+  .route("task/contractor/:id")
   .get(taskController.findByContractorId)
 
 module.exports = router;
